54018a2a03f937781a3d900dd079c8c184503f8c
tobybrad@microsoft.com <8226225+tobybrad@users.noreply.github.com>
2020-07-10 20:23:39 +0100

    resource_group_location == location

--- scripts/automation/shared_services-ci.sh
+++ scripts/automation/shared_services-ci.sh
@@ -3,6 +3,8 @@ set -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $DIR/terraform-checkvars.sh
 
+export TF_VAR_resource_group_location=$TF_VAR_location
+
 terraform init
 terraform fmt
 terraform validate
