7836f3295a1c6a11268a285f908b6ed006692616
tobybrad@microsoft.com <8226225+tobybrad@users.noreply.github.com>
2020-07-10 19:24:04 +0100

    Tidy up the pipeline scripts

--- scripts/automation/deploy_shared_services.sh
+++ scripts/automation/deploy_shared_services.sh
@@ -4,4 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $DIR/terraform-checkvars.sh
 
 export TF_VAR_suffix=[\"$TF_VAR_environment_id\"]
+export TF_VAR_authorized_security_client_ips=[\"$(dig +short myip.opendns.com @resolver1.opendns.com)\"]
 terraform init -backend-config=./backend.config && terraform plan && terraform apply -auto-approve
