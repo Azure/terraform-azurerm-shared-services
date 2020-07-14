edf2a480b783cde09224b3dee6816bc17e8eb98b
tobybrad@microsoft.com <8226225+tobybrad@users.noreply.github.com>
2020-07-14 14:46:06 +0100

    Set build agent subnet to add to KV FW

--- scripts/automation/shared_services-cd.sh
+++ scripts/automation/shared_services-cd.sh
@@ -1,7 +1,8 @@
-set -ex
+set -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $DIR/terraform-checkvars.sh
+ 
+export TF_VAR_authorized_audit_subnet_ids=$BUILD_AGENT_SUBNET_ID
+$DIR/deploy_remote_backend.sh && $DIR/deploy_shared_services.sh
 
-$DIR/deploy_remote_backend.sh
-$DIR/deploy_shared_services.sh
