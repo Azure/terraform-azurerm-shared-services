9ea046c2c7ea524a278642685d99bbf5e9cb9912
tobybrad@microsoft.com <8226225+tobybrad@users.noreply.github.com>
2020-07-17 16:22:24 +0100

    New backend bootstrapping approach

--- bootstrap/agent_installer.sh
+++ bootstrap/agent_installer.sh
@@ -1,6 +1,8 @@
 AGENT_VERSION=2.172.0
 AGENT_ARCHIVE=vsts-agent-linux-x64-$AGENT_VERSION.tar.gz
 
+# Install build agent
+
 mkdir -p agent
 cd agent
 wget -O $AGENT_ARCHIVE https://vstsagentpackage.azureedge.net/agent/$AGENT_VERSION/$AGENT_ARCHIVE
@@ -9,3 +11,22 @@ tar xvfz $AGENT_ARCHIVE
 AGENT_ALLOW_RUNASROOT="1" ./config.sh --unattended --url ${ORG}  --auth pat --token ${PAT} --pool default --agent ${NAME}  --acceptTeeEula
 ./svc.sh install
 ./svc.sh start
+
+# Install Docker
+
+apt-get update
+apt-get -y autoremove docker docker-engine docker.io containerd runc
+apt-get -y install 
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
+
+# Install Azure CLI
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+
+# Install build-essential
+
+apt-get -y install build-essential
