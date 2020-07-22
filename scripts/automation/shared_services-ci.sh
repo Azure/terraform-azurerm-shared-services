06fce2f79bc1519c39ffc5c1d5bba3f15a87b42f
tobybrad@microsoft.com <8226225+tobybrad@users.noreply.github.com>
2020-07-22 15:36:02 +0100

    Separate the pipelines

--- scripts/automation/shared_services-ci.sh
+++ scripts/automation/shared_services-ci.sh
@@ -1,7 +1,7 @@
 set -ex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-$DIR/terraform-checkvars.sh
+$DIR/terraform-checkvars-ci.sh
 
 export TF_VAR_resource_group_location=$TF_VAR_location
 
