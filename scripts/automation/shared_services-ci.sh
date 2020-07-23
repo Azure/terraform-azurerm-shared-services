a8322810234f53337f9b43c38f35278c916bb8b9
tobybrad@microsoft.com <8226225+tobybrad@users.noreply.github.com>
2020-07-23 16:53:22 +0100

    suffix == environment_id

--- scripts/automation/shared_services-ci.sh
+++ scripts/automation/shared_services-ci.sh
@@ -3,6 +3,7 @@ set -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $DIR/terraform-checkvars-ci.sh
 
+export TF_VAR_suffix=[\"$TF_VAR_environment_id\"]
 export TF_VAR_resource_group_location=$TF_VAR_location
 
 terraform init
