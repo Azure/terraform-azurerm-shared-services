1fd304e0187c3a425cbd3b08ea7e4cc8a4593940
tobybrad@microsoft.com <8226225+tobybrad@users.noreply.github.com>
2020-07-23 14:48:38 +0100

    Consolidate docker build pieces

--- scripts/automation/docker-ci.sh
+++ scripts/automation/docker-ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 if [ -z ${DOCKER_REGISTRY_URL+x} ]; then
     echo "DOCKER_REGISTRY_URL is required"
@@ -10,23 +10,28 @@ if [ -z ${DOCKER_IMAGE_NAME+x} ]; then
     echo "DOCKER_IMAGE_NAME is required"
     exit 1
 fi
-if [ -z ${DOCKER_IMAGE_TAG+x} ]; then
-    echo "DOCKER_IMAGE_TAG is required"
-    exit 1
-fi
 if [ -z ${DOCKERFILE_PATH+x} ]; then
     echo "DOCKERFILE_PATH is required"
     exit 1
 fi
 
-DOCKER_IMAGE="${DOCKER_REGISTRY_URL}/${DOCKER_IMAGE_NAME}"
-DOCKER_IMAGE_WITH_TAG="${DOCKER_IMAGE}:${DOCKER_IMAGE_TAG}${DOCKER_IMAGE_TAG_EXT}"
-DOCKER_IMAGE_LATEST="${DOCKER_IMAGE}:latest"
+# Only build if .devcontainer has changed
+#
+if [[ -z $(git diff @~..@ .devcontainer/) ]]; then
+  DOCKER_IMAGE_TAG=$(git rev-parse --short HEAD)
+  DOCKER_IMAGE="${DOCKER_REGISTRY_URL}/${DOCKER_IMAGE_NAME}"
+  DOCKER_IMAGE_WITH_TAG="${DOCKER_IMAGE}:${DOCKER_IMAGE_TAG}"
+  DOCKER_IMAGE_LATEST="${DOCKER_IMAGE}:latest"
+fi
 
 echo "Building ${DOCKER_IMAGE}"
 docker build -t "${DOCKER_IMAGE_WITH_TAG}" -f "${DOCKERFILE_PATH}" .
 
-echo "Publishing ${DOCKER_IMAGE}"
-docker push "${DOCKER_IMAGE_WITH_TAG}"
-docker tag "${DOCKER_IMAGE_WITH_TAG}" "${DOCKER_IMAGE_LATEST}"
-docker push "${DOCKER_IMAGE_LATEST}"
+# Iff main/master publish
+#
+if [[ $(git rev-parse --abbrev-ref HEAD) == 'master' ]]; then
+  echo "Publishing ${DOCKER_IMAGE}"
+  docker push "${DOCKER_IMAGE_WITH_TAG}"
+  docker tag "${DOCKER_IMAGE_WITH_TAG}" "${DOCKER_IMAGE_LATEST}"
+  docker push "${DOCKER_IMAGE_LATEST}"
+fi
