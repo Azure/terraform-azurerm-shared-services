90b797570c044fd4bdb95104b7544777a23f5e0e
Luke Devonshire <luke@MacBook-Pro.local>
2020-08-21 20:22:05 +0100

    adding example

--- examples/bootstrap/main.tf
+++ examples/bootstrap/main.tf
@@ -6,9 +6,9 @@ provider "azurerm" {
 locals {
   #unique_name_stub          = substr(module.naming.unique-seed, 0, 5)
   unique_name_stub          = "luked"
-  azure_devops_organisation = "https://dev.azure.com/lukedevonshire"
-  azure_devops_project      = "AnalyticsPlatform"
-  azure_devops_PAT          = "g25u6yfar532e35g7md37mb3dzsq4s4wbho4ku6kt4jd3sluqbfa"
+  azure_devops_organisation = 
+  azure_devops_project      = 
+  azure_devops_PAT          = 
 }
 
 module "naming" {
@@ -18,11 +18,11 @@ module "naming" {
 #NOTE: SET THESE ENV VARS MANUALLY BEFORE RUNNING THIS EXAMPLE OTHERWISE THE SAMPLE SCRIPTS WILL FAIL 
 
 #export TF_VAR_environment_id=luked
-#export TF_VAR_devops_pat_token=g25u6yfar532e35g7md37mb3dzsq4s4wbho4ku6kt4jd3sluqbfa 
-#export TF_VAR_devops_org=https://dev.azure.com/lukedevonshire
-#export TF_VAR_devops_project=AnalyticsPlatform
-#export ARM_SUBSCRIPTION_ID=d4bf2856-9a60-4ce4-b41f-423833662915 
-#export ARM_TENANT_ID=72f988bf-86f1-41af-91ab-2d7cd011db47 
+#export TF_VAR_devops_pat_token= 
+#export TF_VAR_devops_org=
+#export TF_VAR_devops_project=
+#export ARM_SUBSCRIPTION_ID=
+#export ARM_TENANT_ID=
 
 module "build_environment" {
   source                      = "../../bootstrap"
