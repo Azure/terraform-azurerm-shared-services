2b97525df21b793c40d58145eada9fe88cc19eba
Luke Devonshire <luke@MacBook-Pro.local>
2020-08-24 17:16:57 +0100

    Refactor shared services

--- submodules/bootstrap/examples/main.tf
+++ submodules/bootstrap/examples/main.tf
@@ -1,11 +1,11 @@
 #NOTE: SET THESE ENV VARS MANUALLY BEFORE RUNNING THIS EXAMPLE OTHERWISE THE SAMPLE SCRIPTS WILL FAIL 
 
-#export TF_VAR_environment_id=luked
-#export TF_VAR_devops_pat_token=tn2qpocl67us3smyut7yh6v74sqmyw6mpzn6h2f3sttvupgchgeq
-#export TF_VAR_devops_org=https://dev.azure.com/lukedevonshire
-#export TF_VAR_devops_project=AnalyticsPlatform
-#export ARM_SUBSCRIPTION_ID=d4bf2856-9a60-4ce4-b41f-423833662915 
-#export ARM_TENANT_ID=72f988bf-86f1-41af-91ab-2d7cd011db47 
+#export TF_VAR_environment_id=
+#export TF_VAR_devops_pat_token=
+#export TF_VAR_devops_org=
+#export TF_VAR_devops_project=
+#export ARM_SUBSCRIPTION_ID= 
+#export ARM_TENANT_ID= 
 
 provider "azurerm" {
   version = "~>2.0"
@@ -20,11 +20,11 @@ module "bootstrap" {
   source                      = "../"
   resource_group_location     = "uksouth"
   virtual_network_cidr        = "10.0.0.0/20"
-  build_agent_admin_username  = "buildadmin"
-  build_agent_admin_password  = "Ais4Alpha!"
-  azure_devops_organisation   = "https://dev.azure.com/lukedevonshire"
-  azure_devops_project        = "AnalyticsPlatform"
-  azure_devops_pat            = "tn2qpocl67us3smyut7yh6v74sqmyw6mpzn6h2f3sttvupgchgeq"
+  build_agent_admin_username  = ""
+  build_agent_admin_password  = ""
+  azure_devops_organisation   = ""
+  azure_devops_project        = ""
+  azure_devops_pat            = ""
   suffix                      = [local.unique_name_stub]
   use_existing_resource_group = false
   resource_group_name         = ""
