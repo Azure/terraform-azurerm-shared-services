753a6e156f5d1da4f7b11e9e7c17e64f93af065b
Luke Devonshire <luke@MacBook-Pro.local>
2020-08-25 11:58:15 +0100

    Removed DevOps vars from CI and CD

--- scripts/automation/terraform-checkvars-cd.sh
+++ scripts/automation/terraform-checkvars-cd.sh
@@ -3,7 +3,7 @@
 set -ex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-$DIR/terraform-checkvars-common.sh && $DIR/terraform-checkvars-bootstrap.sh
+$DIR/terraform-checkvars-common.sh 
 
 fail=$?
 
