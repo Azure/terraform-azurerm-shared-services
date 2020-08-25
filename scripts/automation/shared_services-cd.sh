cbe0c7004a7e64abec89b8fd4ae440716a3afcf9
Luke Devonshire <luke@MacBook-Pro.local>
2020-08-25 14:14:44 +0100

    Changed to deploy NSG to net rg

--- scripts/automation/shared_services-cd.sh
+++ scripts/automation/shared_services-cd.sh
@@ -3,9 +3,10 @@ set -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $DIR/terraform-checkvars-cd.sh
 
+$DIR/create_backend_config.sh
+
 export TF_VAR_suffix=[\"$TF_VAR_suffix\"]
 export TF_VAR_resource_group_location=$TF_VAR_resource_group_location
 
-$DIR/create_backend_config.sh
 terraform init -backend-config=./backend.config && terraform plan && terraform apply -auto-approve
 
--- submodules/build_environment/main.tf
+++ submodules/build_environment/main.tf
@@ -31,7 +31,7 @@ resource "azurerm_subnet" "build" {
 resource "azurerm_network_security_group" "build_nsg" {
   name                = join(module.naming.network_security_group.dashes ? "-" : "", [module.naming.subnet.name])
   location            = azurerm_resource_group.backend.location
-  resource_group_name = azurerm_resource_group.backend.name
+  resource_group_name = var.virtual_network_resource_group_name
 }
 
 resource "azurerm_subnet_network_security_group_association" "build_nsg_asso" {
