89fdf2c746c421276664dfb79d2fde2cd5f307e8
jjcollinge <jonathancollinge@live.com>
2020-08-26 13:20:26 +0100

    update bootstrap state

--- scripts/automation/create_backend_config.sh
+++ scripts/automation/create_backend_config.sh
@@ -1,24 +1,26 @@
+TF_STATE_KEY=${1:-"terraform.tfstate"} 
+
 # Add remote backend
-#
 cat << EOF > backend.tf
 terraform { 
     backend "azurerm" {} 
 }
 EOF
 
+BACKEND_CONFIG="backend.config"
+# Remove existing terraform backend config file if exists
+rm -f $BACKEND_CONFIG
+
+# Get terraform backend config values
 RG_NAME=rg-dev-ss-${TF_VAR_suffix}
 SA_NAME=sadevss${TF_VAR_suffix}
 CT_NAME="terraform-tfstate"
 az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
 SA_KEY=$(az storage account keys list --resource-group $RG_NAME --account-name $SA_NAME --query "[0].value")
 
-# Create fresh backend.config
-
-BACKEND_CONFIG="backend.config"
-
-rm -f $BACKEND_CONFIG
+# Write new terraform backend config file
 echo "resource_group_name=\"$RG_NAME\"" >> $BACKEND_CONFIG
 echo "storage_account_name=\"$SA_NAME\"" >> $BACKEND_CONFIG
 echo "container_name=\"$CT_NAME\"" >> $BACKEND_CONFIG
-echo "key=\"terraform.tfstate\"" >> $BACKEND_CONFIG
+echo "key=\"$TF_STATE_KEY\"" >> $BACKEND_CONFIG
 echo "access_key=$SA_KEY" >> $BACKEND_CONFIG
--- scripts/automation/shared_services-cd.sh
+++ scripts/automation/shared_services-cd.sh
@@ -1,12 +1,16 @@
-set -ex
+set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $DIR/terraform-checkvars-cd.sh
 
+# Create a terraform backend configuration file to store the shared services state
 $DIR/create_backend_config.sh
 
+# Mangle env vars into the correct format for terraform and export
 export TF_VAR_suffix=[\"$TF_VAR_suffix\"]
 export TF_VAR_resource_group_location=$TF_VAR_resource_group_location
 
-terraform init -backend-config=./backend.config && terraform plan && terraform apply -auto-approve
-
+# Initialize terraform using the new shared services backend
+terraform init -backend-config=./backend.config
+terraform plan
+terraform apply -auto-approve
